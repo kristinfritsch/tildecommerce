b8555ee1f327e9fb8f6ad7c2daeab9e913625abf
Ishan Chatterjee <ichatterjee@circle.com>
2020-06-13 17:54:41 -0400

    Support Ropsten monitoring for easier testing

--- js/ethereum/etherscan.js
+++ js/ethereum/etherscan.js
@@ -1,17 +1,45 @@
 class Etherscan {
+  network = "mainnet";
+
+  constructor(network = "mainnet") {
+    this.network = network;
+  }
+
   get apiKey() {
     return "NMP361651P5PUDV2U6XV4H171MVE2RXASZ";
   }
 
+  get apiHost() {
+    switch (this.network) {
+      case "mainnet":
+        return "api.etherscan.io";
+      case "testnet":
+        return "api-ropsten.etherscan.io";
+      default:
+        throw new Error("Unsupported network: " + this.network);
+    }
+  }
+
+  get contractAddress() {
+    switch (this.network) {
+      case "mainnet":
+        return "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+      case "testnet":
+        return "0x07865c6e87b9f70255377e024ace6630c1eaa37f";
+      default:
+        throw new Error("Unsupported network: " + this.network);
+    }
+  }
+
   getAddressBalance(address) {
     return $.getJSON(
-      `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&address=${address}&tag=latest&apikey=${this.apiKey}`
+      `https://${this.apiHost}/api?module=account&action=tokenbalance&contractaddress=${this.contractAddress}&address=${address}&tag=latest&apikey=${this.apiKey}`
     );
   }
 
   getMostRecentTokenTransfers(address, limit = 5) {
     return $.getJSON(
-      `https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&address=${address}&page=1&offset=${limit}&sort=desc&apikey=${this.apiKey}`
+      `https://${this.apiHost}/api?module=account&action=tokentx&contractaddress=${this.contractAddress}&address=${address}&page=1&offset=${limit}&sort=desc&apikey=${this.apiKey}`
     );
   }
 }
